ae1b9fdc2269f16c3e57da5c3a38fff04425d60b
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2020-02-23 16:28:51 +0800

    Updates

---
+++ precache-manifest.588cdab20a6d5e2cde25043d154f9e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdb3c9c280f6403bb9e3e5aff7f9f0e",
+    "url": "/mich-monsters/index.html"
+  },
+  {
+    "revision": "b54c806ec91b791bf0e4",
+    "url": "/mich-monsters/static/css/main.3d8f37ca.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/mich-monsters/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mich-monsters/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54c806ec91b791bf0e4",
+    "url": "/mich-monsters/static/js/main.98f5fbeb.chunk.js"
+  },
+  {
+    "revision": "4ca8bd8e4848913cc733",
+    "url": "/mich-monsters/static/js/runtime-main.50c864b1.js"
+  }
+]);
